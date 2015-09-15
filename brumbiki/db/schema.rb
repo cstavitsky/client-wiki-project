5c4c33402090737842d8f660af0efeb7b61b291f
Bridget Cahill & Cat McLoughlin & Chris Stavitsky &   "name": null <pair+bcahill.catmclough.cstavitsky.pbsugg@devbootcamp.com>
2015-09-15 18:19:48 -0500

    first commit

--- brumbiki/db/schema.rb
+++ brumbiki/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150914194333) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.integer  "group_id"
     t.datetime "created_at", null: false
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 20150914194333) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
     t.integer  "article_id"
