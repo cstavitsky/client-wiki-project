a50c93de0372c060b158fa2bc0fec470f103aa16
Philip Sugg <pbensugg@gmail.com>
2015-09-15 01:38:37 -0500

    seeds, some with Faker data

--- brumbiki/db/schema.rb
+++ brumbiki/db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20150914194333) do
     t.datetime "updated_at",      null: false
   end
 
-
   add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "versions", force: :cascade do |t|
