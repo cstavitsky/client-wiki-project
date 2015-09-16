685045d83b5df25317db7b11167949d713a92ec3
Cat McLoughlin <catmclough@gmail.com>
2015-09-16 09:50:33 -0500

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,10 +5,10 @@ class Group < ActiveRecord::Base
 
   validates :name, uniqueness: true
 
-  def admin
-    admin_id = self.memberships.find_by(access_level: "admin").user_id
-    User.find_by(id: admin_id)
-  end
+  # def admin
+  #   admin_id = self.memberships.find_by(access_level: "admin").user_id
+  #   User.find_by(id: admin_id)
+  # end
 
   def has_member(user)
     self.users.find_by(id: user.id) != nil
