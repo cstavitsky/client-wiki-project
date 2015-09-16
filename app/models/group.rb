2dd926ec972e5c950cc39f65cf434345346d9f9b
Cat McLoughlin <catmclough@gmail.com>
2015-09-15 22:09:28 -0500

    Add gold star for user's groups and remove 'join group' button on groups where user is already a member

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,13 @@ class Group < ActiveRecord::Base
   has_many :users, through: :memberships
 
   validates :name, uniqueness: true
+
+  def admin
+    admin_id = self.memberships.find_by(access_level: "admin").user_id
+    User.find_by(id: admin_id)
+  end
+
+  def has_member(user)
+    !(self.users.where(id: user.id).empty?)
+  end
 end
