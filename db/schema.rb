6147d34b9edd13597da1e813ebaec25130c32970
Philip Sugg <pbensugg@gmail.com>
2015-09-16 15:43:05 -0500

    last add

--- db/schema.rb
+++ db/schema.rb
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20150914194333) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "full_name"
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "full_name",       null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
